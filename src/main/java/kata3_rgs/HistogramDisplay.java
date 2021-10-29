227ef5c4da040e182b9ae566a43616c97dc84216
Admin <Admin@DESKTOP-UG0GOA7>
2021-10-29 14:19:06 +0100

    Version 1.0 kata3 29/10/2021

---
+++ src/main/java/kata3_rgs/HistogramDisplay.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata3_rgs;
+
+import org.jfree.ui.ApplicationFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class HistogramDisplay extends ApplicationFrame {
+    
+    public HistogramDisplay(String title) {
+        super(title);
+    }
+    
+}
